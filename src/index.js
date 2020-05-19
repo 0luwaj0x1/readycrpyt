f5194835f971472a6ad9ed934aaab4ed2f8091f5
jilesanmi <ilesanmi.josiah@gmail.com>
2020-05-19 01:37:07 +0100

    linter and prettier

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const CryptoJS = require('crypto-js')
 
-
-
-const encryptPassword = password =>  CryptoJS.enc.Hex.stringify(CryptoJS.SHA256(password))
- 
+const encryptPassword = password =>
+  CryptoJS.enc.Hex.stringify(CryptoJS.SHA256(password))
 
 const encryptyPin = (pin, key) => {
-  const encKey =  CryptoJS.enc.Utf8.parse(key);
-  const result = CryptoJS.TripleDES.encrypt(pin, encKey,{mode:CryptoJS.mode.ECB});
-  return CryptoJS.enc.Hex.stringify(result.ciphertext);
+  const encKey = CryptoJS.enc.Utf8.parse(key)
+  const result = CryptoJS.TripleDES.encrypt(pin, encKey, {
+    mode: CryptoJS.mode.ECB,
+  })
+  return CryptoJS.enc.Hex.stringify(result.ciphertext)
 }
-
-
 
 module.exports = {
   encryptPassword,
-  encryptyPin
+  encryptyPin,
 }
